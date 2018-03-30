0b1fa49ccd788fe943a251a9cda85415dc1b6412
PrachiJulka <prachijulka31@gmail.com>
2018-03-30 16:17:25 +0530

    Update TopicControllerSpec.groovy

--- Linksharing/src/test/groovy/com/ttn/linksharing/TopicControllerSpec.groovy
+++ Linksharing/src/test/groovy/com/ttn/linksharing/TopicControllerSpec.groovy
@@ -14,30 +14,13 @@ class TopicControllerSpec extends Specification implements ControllerUnitTest<To
     def cleanup() {
     }
 
-   /* def "displays topics on show action"(){
-        given:
-        User user = new User(email: " admin@gmail.com", userName: "PrachiJulka", password: "ROOT@123",
-                firstName: "admin", lastName: "portal", admin: false, active: true, photo: 121)
-
-
-        Topic topic=new Topic(name: "topic20",Visibility:Visibility.PUBLIC,createdBy: session.user)
-        user.addToTopics(topic)
-        topic.save()
-        user.save()
-        session.user=user
-        when:
-        controller.show()
-        then:
-        response.contentAsString==session.user.topics
-
-    }*/
-    /*  void "topic should be saved on save action"() {
+    def "topic should be saved on save action"() {
           given:
           User user = new User(email: "prachijulka@gmail.com", userName: "PrachiJulka", password: "ROOT@123",
                   firstName: "Prachi", lastName: "Julka", admin: false, active: true, photo: 101)
 
               session.user=user
-          Topic topic=new Topic(name: "topic20",Visibility:Visibility.PUBLIC,createdBy: session.user)
+          Topic topic=new Topic(name: "topic20",visibility:Visibility.PUBLIC,createdBy: session.user)
           when:
           controller.save(topic, "Casual")
 
@@ -47,7 +30,6 @@ class TopicControllerSpec extends Specification implements ControllerUnitTest<To
 
 
       }
-  */
 
     void "test something"() {
         expect:"fix me"
